7d43e67d722ae4546e08824cacf3706634f16429
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 20:18:15 +0300

    First Commit

---
+++ ECOM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECOM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

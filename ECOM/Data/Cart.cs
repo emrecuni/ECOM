42bbca7f6fa2a956177c3e617a2f582b92be0bee
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:55:27 +0300

    cart class added

---
+++ ECOM/Data/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class Cart
+    {
+        [Key]
+        [Column("Id")]
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int SellerId { get; set; }
+        public int DCouponId { get; set; }
+        public int Piece { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool Enable { get; set; }
+    }
+}

11dec12ace8624d1df34db538618a15e6d0f294f
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:33:18 +0300

    product categories class added

---
+++ ECOM/Data/ProductCategories.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class ProductCategories
+    {
+        [Key]
+        [Column("Id")]
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public bool Type { get; set; }
+        public DateTime MyProperty { get; set; }
+    }
+}

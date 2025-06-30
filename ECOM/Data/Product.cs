b6d70734e37099198d7f8a31141938f8d6f4f337
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:26:00 +0300

    product class added

---
+++ ECOM/Data/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class Product
+    {
+        [Key]
+        [Column("Id")]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+        public string Description { get; set; }
+        public int SupCategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+        public float Score { get; set; }
+        public DateTime AdditionTime { get; set; }
+    }
+}

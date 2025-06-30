46cd5e608a28797397f966e5cda0b4413b59c24e
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 21:50:51 +0300

    product controller added

---
+++ ECOM/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECOM.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendComment(string comment, int rate)
+        {
+            ViewBag.Comment = comment;
+            ViewBag.Rate = rate;
+            ViewBag.CommentInfo = "Yorum Başarıyla Eklendi.";
+            return View("Index");
+        }
+
+        public IActionResult AddCart(string productName, float price) // seçilen ürünü sepete ekler
+        {
+            ViewBag.CartProductName = productName;
+            ViewBag.CartPrice = price;
+            return View("Index");
+        }
+
+        public IActionResult Buy(string productName, float price) // seçilen ürünü doğrudan satın alma ekranına yönlendirir
+        {
+
+            // direkt satın alma ekranın yönlendir
+            ViewBag.BuyProductName = productName;
+            ViewBag.BuyPrice = price;
+            return View("Index");
+        }
+    }
+}

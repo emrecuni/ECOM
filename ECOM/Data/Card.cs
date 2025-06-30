053353c09b43c1e42ddc7ad0e5fc17d08e41cd58
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:21:22 +0300

    card class added

---
+++ ECOM/Data/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class Card
+    {
+        [Key]
+        [Column("Id")]
+        public int CardId { get; set; }
+        public int CustomerId { get; set; }
+        public string? CardNo { get; set; }
+        public string? ExpirationDate { get; set; }
+        public string? CVV { get; set; }
+        public DateTime AdditionTime { get; set; }
+    }
+}

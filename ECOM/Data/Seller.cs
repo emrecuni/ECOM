d45d53830f9a1439fba77e06d1bf7906b7541df9
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:44:11 +0300

    seller class added

---
+++ ECOM/Data/Seller.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class Seller
+    {
+        [Key]
+        [Column("Id")]
+        public int SellerId { get; set; }
+        public string? Name { get; set; }
+        public float Score { get; set; }
+        public DateTime AdditionTime { get; set; }
+    }
+}

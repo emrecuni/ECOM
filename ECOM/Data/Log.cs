190f73f571ff1518ee4dae3521e2253f409d2826
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:42:41 +0300

    log class added

---
+++ ECOM/Data/Log.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class Log
+    {
+        [Key]
+        [Column("Id")]
+        public int LogId { get; set; }
+        public string? TableName { get; set; }
+        public string? OldValue { get; set; }
+        public string? NewValue { get; set; }
+        public char ProcessType { get; set; }
+        public DateTime ProcessTime { get; set; }
+    }
+}

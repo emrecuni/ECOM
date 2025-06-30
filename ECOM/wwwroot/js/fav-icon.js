2d3785500fff2f745d53c44a9ed03b54c99871a6
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:04:21 +0300

    fav-icon.js added

---
+++ ECOM/wwwroot/js/fav-icon.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const favoriteButtons = document.querySelectorAll('.favorite-btn');
+
+    favoriteButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const icon = button.querySelector('i');
+            icon.style.color = icon.style.color === 'red' ? '' : 'red'; // Renk değişimi
+        });
+    });
+});

117fdc431d2a588f1941b19ebb1def23b0cd6678
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 20:49:34 +0300

    login controller added

---
+++ ECOM/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECOM.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username != null && password != null)
+            {
+                if (username == "admin" && password == "admin")
+                    return RedirectToAction("Index", "Main");
+
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            ViewBag.ForgotPassword = true;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string email) // parola sıfırlama isteği girilen mail adresine girilmelidir
+        {
+            ViewBag.Info = "Parola sıfırlama isteği gönderildi.";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string name, string surname, string email, string password) // parametreleri modele dönüştür
+        {
+            ViewBag.Info = "Yeni Kayıt Başarılı";
+            return View("Index");
+        }
+    }
+}

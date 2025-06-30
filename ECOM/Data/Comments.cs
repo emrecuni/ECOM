16d70c1dbeefbd40fe745ebd4b098af88bf90a76
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:53:06 +0300

    comments class added

---
+++ ECOM/Data/Comments.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class Comments
+    {
+        [Key]
+        [Column("Id")]
+        public int CommentID { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public string? Comment { get; set; }
+        public string? ImagePath { get; set; }
+
+    }
+}

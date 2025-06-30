37a188edeb30e6a0cbaa77773d078ad19a9e510c
Emre Cüni <cuniiemre@gmail.com>
2025-06-30 22:46:42 +0300

    city class added

---
+++ ECOM/Data/City.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Data
+{
+    public class City
+    {
+        [Key]
+        [Column("Id")]
+        public int CityId { get; set; }
+        public string? Name { get; set; }
+    }
+}

264531c2c2f13110a5953b1d4291f1d7a0f3c533
Emre Cüni <cuniiemre@gmail.com>
2025-07-02 23:28:45 +0300

    data context update

--- ECOM/Data/DataContext.cs
+++ ECOM/Data/DataContext.cs
@@ -7,6 +7,23 @@ namespace ECOM.Data
         public DataContext(DbContextOptions options) : base(options)
         {
 
-        }        
+        }
+        public DbSet<Addresses> Addresses => Set<Addresses>();
+        public DbSet<Brand> Brands => Set<Brand>();
+        public DbSet<Card> Cards => Set<Card>();
+        public DbSet<Cart> Carts => Set<Cart>();
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Comments> Comments => Set<Comments>();
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<DCoupon> DCoupons => Set<DCoupon>();
+        public DbSet<District> Districts => Set<District>();
+        public DbSet<Favorites> Favorites => Set<Favorites>();
+        public DbSet<Log> Log => Set<Log>();
+        public DbSet<Neighbourhood> Neighbourhoods => Set<Neighbourhood>();
+        public DbSet<OrderHistory> OrderHistory => Set<OrderHistory>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<ProductCategories> ProductCategories => Set<ProductCategories>();
+        public DbSet<SCoupon> SCoupons => Set<SCoupon>();
+        public DbSet<Seller> Sellers => Set<Seller>();
     }
 }

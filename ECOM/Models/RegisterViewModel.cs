d931de5de083c9f44e81a747ad6e5df89a32a819
Emre Cüni <cuniiemre@gmail.com>
2025-07-12 22:41:44 +0300

    register updated

---
+++ ECOM/Models/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECOM.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Phone { get; set; }
+
+        [Required]
+        public bool Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+    }
+
+}

85e21faa012da3a1b241d93e70b0e715329e91f1
Emre Cüni <cuniiemre@gmail.com>
2025-07-20 23:01:56 +0300

    cart bug fix

---
+++ ECOM/Views/Cart/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ECOM.Data;
+
+namespace ECOM.Views.Cart
+{
+    public class CartViewModel
+    {
+        public List<Data.Cart> Carts { get; set; } = [];
+        public List<Favorites> Favorites { get; set; } = [];
+    }
+}

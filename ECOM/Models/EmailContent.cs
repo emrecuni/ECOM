1cfb51c088eabfddf78907035c7531cd4b0a243a
Emre Cüni <cuniiemre@gmail.com>
2025-10-08 20:15:24 +0300

    emailcontent class added

---
+++ ECOM/Models/EmailContent.cs
@@ -0,0 +1,10 @@
+﻿namespace ECOM.Models
+{
+    public class EmailContent
+    {
+        public string ToMail { get; set; } = null!;
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+        public int Expire { get; set; }
+    }
+}

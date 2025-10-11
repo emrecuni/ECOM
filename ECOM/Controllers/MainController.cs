d8de5547b4b4bb422e97d3f2eb7f5c45771c2043
Emre Cüni <cuniiemre@gmail.com>
2025-10-11 19:27:42 +0300

    favorite added

--- ECOM/Controllers/MainController.cs
+++ ECOM/Controllers/MainController.cs
@@ -1,8 +1,10 @@
 ﻿using ECOM.Data;
+using ECOM.Models;
 using ECOM.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ECOM.Controllers
@@ -10,10 +12,12 @@ namespace ECOM.Controllers
     public class MainController : Controller
     {
         private readonly DataContext _context;
+        private readonly ILogger<MainController> _logger;
 
-        public MainController(DataContext context)
+        public MainController(DataContext context, ILogger<MainController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [Authorize]
@@ -107,6 +111,48 @@ namespace ECOM.Controllers
             {
                 NLogger.logger.Error($"SearchProductByName Error => {ex}");
                 return View("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Favorite (int id)
+        {
+            try
+            {
+                int customerId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+                bool isFavorite;
+                var favorite = await _context.Favorites.FirstOrDefaultAsync(f => f.CustomerId == customerId && f.ProductId == id);
+
+                if(favorite is null) // favorilerde yoksa eklenir
+                {
+                    _context.Favorites.Add(favorite = new Favorites
+                    {
+                        CustomerId = customerId,
+                        ProductId = id,
+                        AdditionTime = DateTime.Now
+                    });
+                    isFavorite = true;
+                }
+                else // favorilerde varsa silinir
+                {
+                    _context.Favorites.Remove(favorite);
+                    isFavorite = false;
+                }
+                await _context.SaveChangesAsync(); // veri tabanına kaydedilir
+                
+                return Json(new { isFavorite });
+            }
+            catch (Exception ex)
+            {
+                Guid guid = Guid.NewGuid();
+                ErrorViewModel error = new ErrorViewModel
+                {
+                    Message = "Hesap bilgileri getirilirken bir hata oluştu.",
+                    RequestId = HttpContext.TraceIdentifier,
+                    Title = $"Hata Kodu: {guid}"
+                };
+                _logger.LogError($"Main/Favorite Error Hata Kodu: {guid} => {ex}");
+                return View("Error", error);
             }
         }
     }

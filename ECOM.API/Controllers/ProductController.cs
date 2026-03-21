aa6dc8c388d2d63cb63322db8c8fa0c2b9496f47
devE <cuniiemre@gmail.com>
2026-03-21 16:47:03 +0300

    Product Controller and Interface Added

---
+++ ECOM.API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using ECOM.API.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECOM.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(IProductService productService,ILogger<ProductController> logger)
+        {
+            _productService = productService;
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

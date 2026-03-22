0aee023d9f8b88517fbe40460232b5bba8f82f57
devE <cuniiemre@gmail.com>
2026-03-22 12:48:07 +0300

    ProductDtos Added

---
+++ ECOM.Shared/Data/DTOs/BasicProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECOM.Shared.Data.Entities;
+
+namespace ECOM.Shared.Data.DTOs
+{
+    public class BasicProductDto
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public float? Score { get; set; }
+        public string? ImagePath { get; set; }
+        public ICollection<Favorites> Favorites { get; set; } = [];
+    }
+}

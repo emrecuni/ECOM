44f5bb881e22c645f051b121730d12ae6e6b8669
emrecuni <cuniiemre@gmail.com>
2026-07-03 13:58:22 +0300

    callback dtos added

---
+++ ECOM.Shared/Data/DTOs/Payment/CallbackRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace ECOM.Shared.Data.DTOs.Payment
+{
+    public class CallbackRequestDto
+    {
+        public IFormCollection? Form { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ ECOM.Shared/Data/DTOs/Payment/CallbackResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECOM.Shared.Data.DTOs.Payment
+{
+    public class CallbackResponseDto
+    {
+        public string? PaymentStatus { get; set; }
+        public string? PaymentId { get; set; }
+        public string? ResultMessage { get; set; }
+        public decimal Price { get; set; }
+    }
+}

545a1a72f36816fd5440016c8e59df08516789c0
Emre Cüni <cuniiemre@gmail.com>
2026-07-08 22:59:46 +0300

    fields marked as required

--- ECOM.Shared/Data/DTOs/Auth/LoginRequestDto.cs
+++ ECOM.Shared/Data/DTOs/Auth/LoginRequestDto.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ECOM.Shared.Data.DTOs.Auth
 {
     public class LoginRequestDto
     {
+        [Required(ErrorMessage = "Email zorunludur.")]
+        [MaxLength(200)]
         public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Parola zorunludur.")]
+        [MaxLength(50)]
         public string Password { get; set; } = null!;
     }
 }
